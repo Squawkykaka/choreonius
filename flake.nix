7efd159a90d65b79088b6a0823104822786c5f1e
Squawkykaka <squawkykaka@gmail.com>
2025-09-08 21:14:22 +1200

    initial setup

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "Build a cargo project without extra checks";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
+
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    naersk,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [(import rust-overlay)];
+        };
+
+        toolchain = pkgs.rust-bin.selectLatestNightlyWith (
+          toolchain:
+            toolchain.default.override {
+              extensions = [
+                "rust-src"
+              ];
+            }
+        );
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+      in {
+        packages.default = naersk'.buildPackage {
+          pname = "siege-week-2";
+          src = ./.;
+          buildInputs = [
+            pkgs.openssl
+            pkgs.pkg-config
+          ];
+        };
+
+        devShells.default = pkgs.mkShell {
+          # Additional dev-shell environment variables can be set directly
+          # RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+          RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+
+          # Extra inputs can be added here; cargo and rustc are provided by default.
+          packages = [
+            pkgs.rust-analyzer
+            pkgs.openssl
+            pkgs.pkg-config
+            toolchain
+          ];
+        };
+      }
+    );
+}
